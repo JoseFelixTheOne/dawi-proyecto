881da1245c1ef91a767e3d5a78d6e3c8fbbf9bb6
Jose Felix <torrestjfelix@gmail.com>
2023-06-11 17:29:56 -0500

    Insert probado y funcional

--- src/main/java/com/dawi/controller/TipoUsuarioController.java
+++ src/main/java/com/dawi/controller/TipoUsuarioController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.dawi.models.TipoUsuario;
 import com.dawi.repository.ITipoPersonaRepository;
@@ -30,10 +32,26 @@ public class TipoUsuarioController {
 		return lista;
 	}
 	@GetMapping("/tipousuario")
-	public String grudTipoUsuario(Model model) {
-		System.out.println("Entro al metodo");
+	public String crudTipoUsuario(Model model) {
 		model.addAttribute("tipos", listarTiposUsuario());
 		model.addAttribute("tipo", new TipoUsuario());
+		return "crudtipo";
+	}
+	@PostMapping("/tipousuario")
+	public String guardarTipo(@ModelAttribute TipoUsuario tipo ,Model model) {
+		String mensaje = "";
+		tipo.setActivo_tipousu("a");
+		try {
+			System.out.println("Entro al metodo");
+			tipoUsuRepo.save(tipo);
+			mensaje = "Registro exitoso";
+			
+		} catch (Exception e) {
+			mensaje = "Error al guardar";
+		}
+		model.addAttribute("tipos", listarTiposUsuario());
+		model.addAttribute("tipo", new TipoUsuario());
+		model.addAttribute("mensaje", mensaje);
 		return "crudtipo";
 	}
 }
