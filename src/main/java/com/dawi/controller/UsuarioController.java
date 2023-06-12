5fd49029b32a7bb94444957030ba36d9f7e281e0
elifio <elifioroldan@hotmail.com>
2023-06-12 02:02:15 -0500

    Trayendo data

--- src/main/java/com/dawi/controller/UsuarioController.java
+++ src/main/java/com/dawi/controller/UsuarioController.java
@@ -8,8 +8,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.dawi.models.Persona;
+import com.dawi.models.TipoUsuario;
 import com.dawi.models.Usuario;
 import com.dawi.repository.IPersonaRepository;
+import com.dawi.repository.ITipoPersonaRepository;
 import com.dawi.repository.IUsuarioRepository;
 
 @Controller
@@ -18,12 +20,21 @@ public class UsuarioController {
 	
 	@Autowired
 	private IUsuarioRepository usuarioRepo;
-	
+	@Autowired
+	private IPersonaRepository personaRepo;
+	@Autowired
+	private ITipoPersonaRepository tipoUsuRepo;
+
 	
 	@GetMapping("/usuario")
 	public String cargar(Model model) {
+		TipoUsuarioController oTipoUsuarioController=new TipoUsuarioController();
+		PersonaController oPersonaController=new PersonaController();
 		model.addAttribute("usuarios",listarUsuarios());
 		model.addAttribute("usuario", new Usuario());
+		model.addAttribute("personas", listarPersonas());
+		model.addAttribute("tipousuarios", listarTiposUsuario());
+
 		return "crudusuario";
 	}
 	
@@ -34,6 +45,31 @@ public class UsuarioController {
 			if(usuario.getActivo_usu().equals("a")) {
 				lista.add(usuario);
 			}
+		}
+		return lista;
+	}
+	
+	private ArrayList<Persona> listarPersonas() {
+		ArrayList<Persona> lista=new ArrayList<Persona> ();
+		var personas=personaRepo.findAll();
+		for (Persona persona : personas) {
+			if(persona.getActivo_per().equals("a")) {
+				lista.add(persona);
+			}
+		}
+		return lista;
+	}
+	
+	private ArrayList<TipoUsuario> listarTiposUsuario(){
+		ArrayList<TipoUsuario> lista = new ArrayList<TipoUsuario>();
+		try {
+			var tipos = tipoUsuRepo.findAll();
+			for(TipoUsuario tipo: tipos) {
+				if(tipo.getActivo_tipousu().equals("a"))
+					lista.add(tipo);
+			}
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
 		}
 		return lista;
 	}
