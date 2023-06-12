d666c58aab64eee52e67035fd4621fc08771d7e2
elifio <elifioroldan@hotmail.com>
2023-06-12 11:43:16 -0500

    Creando el insert

--- src/main/java/com/dawi/controller/UsuarioController.java
+++ src/main/java/com/dawi/controller/UsuarioController.java
@@ -1,11 +1,16 @@
 package com.dawi.controller;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.dawi.models.Persona;
 import com.dawi.models.TipoUsuario;
@@ -42,7 +47,7 @@ public class UsuarioController {
 		ArrayList<Usuario> lista=new ArrayList<Usuario> ();
 		var usuarios=usuarioRepo.findAll();
 		for (Usuario usuario : usuarios) {
-			if(usuario.getActivo_usu().equals("a")) {
+			if(usuario.getActivo_usu().equals("a") && usuario.getOpersona().getActivo_per().equals("a")) {
 				lista.add(usuario);
 			}
 		}
@@ -53,7 +58,7 @@ public class UsuarioController {
 		ArrayList<Persona> lista=new ArrayList<Persona> ();
 		var personas=personaRepo.findAll();
 		for (Persona persona : personas) {
-			if(persona.getActivo_per().equals("a")) {
+			if(persona.getActivo_per().equals("a") && persona.getBtieneusuario()==0 ) {
 				lista.add(persona);
 			}
 		}
@@ -73,6 +78,60 @@ public class UsuarioController {
 		}
 		return lista;
 	}
+	
+	
+	@PostMapping("/usuario")
+	public String guardarUsuario(@ModelAttribute Usuario usuario,Model model) {
+		String mensaje="";
+		usuario.setActivo_usu("a");
+        try {
+        	if(usuario.getId_usu()==0) {
+        		String contra= hashSHA256(usuario.getContra_usu());
+        		usuario.setContra_usu(contra);
+            	usuarioRepo.save(usuario);        		
+        		Persona opersona= personaRepo.findById(usuario.getId_per()).orElse(new Persona());
+        		opersona.setBtieneusuario(1);
+        		personaRepo.save(opersona);
+
+        	}else {
+        		Usuario ousuario= usuarioRepo.findById(usuario.getId_usu()).orElse(new Usuario());
+        		ousuario.setNom_usu(ousuario.getNom_usu());
+        		ousuario.setId_tipousu(ousuario.getId_tipousu());
+            	usuarioRepo.save(usuario);        		
+        	}
+            mensaje = "Registro exitosa";
+        } catch(Exception e) {
+            mensaje = "Error al guardar";
+        }
+    	model.addAttribute("usuarios",listarUsuarios());
+		model.addAttribute("usuario", new Usuario());
+		model.addAttribute("personas", listarPersonas());
+		model.addAttribute("tipousuarios", listarTiposUsuario());
+
+		return "crudusuario";
+	}
+	
+	
+	  public String hashSHA256(String input) {
+	        try {
+	            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+	            byte[] encodedHash = digest.digest(input.getBytes(StandardCharsets.UTF_8));
+
+	            StringBuilder hexString = new StringBuilder();
+	            for (byte b : encodedHash) {
+	                String hex = Integer.toHexString(0xff & b);
+	                if (hex.length() == 1) {
+	                    hexString.append('0');
+	                }
+	                hexString.append(hex);
+	            }
+	            return hexString.toString();
+	        } catch (NoSuchAlgorithmException e) {
+	            e.printStackTrace();
+	        }
+	        return null;
+	    }
+	
 	
 	
 }
