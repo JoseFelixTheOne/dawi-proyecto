b57defbc88b4b2a88cf5fb316a6183b3a237ab80
elifio <elifioroldan@hotmail.com>
2023-06-18 12:19:35 -0500

    Interfaz Filtro

--- src/main/java/com/dawi/controller/ReporteController.java
+++ src/main/java/com/dawi/controller/ReporteController.java
@@ -1,6 +1,7 @@
 package com.dawi.controller;
 
 import java.io.OutputStream;
+import java.util.HashMap;
 
 import javax.sql.DataSource;
 
@@ -8,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.servlet.http.HttpServletResponse;
 import net.sf.jasperreports.engine.JasperExportManager;
@@ -31,6 +34,25 @@ public class ReporteController {
 		try {
 		String ru = resourceLoader.getResource("classpath:reportes/reporte_persona.jasper").getURI().getPath();
 		JasperPrint jasperPrint = JasperFillManager.fillReport(ru, null, dataSource.getConnection());
+		OutputStream outStream = response.getOutputStream();
+		JasperExportManager.exportReportToPdfStream(jasperPrint, outStream);
+		} catch (Exception e) {
+		e.printStackTrace();
+		}
+
+	}
+	
+	
+	@PostMapping("/reports/filtro")
+	public void generarPDFListadoFiltro(HttpServletResponse response,@RequestParam(name="txtnombrecompleto") String valor) {
+		response.setHeader("Content-Disposition", "attachment; filename=\"reporte.pdf\";");
+		response.setContentType("application/pdf");
+		try {
+		String ru = resourceLoader.getResource("classpath:reportes/reporte_persona_filtro.jasper").getURI().getPath();
+		HashMap parametros = new HashMap<>();
+		parametros.put("p_nombrecompleto", valor);
+		// la categoria se obtendrá del combo
+		JasperPrint jasperPrint = JasperFillManager.fillReport(ru, parametros, dataSource.getConnection());
 		OutputStream outStream = response.getOutputStream();
 		JasperExportManager.exportReportToPdfStream(jasperPrint, outStream);
 		} catch (Exception e) {
